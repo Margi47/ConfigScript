5a17b4879eb1e89fdd5943f47c91f02bd8fadcc6
Margi <Margi47@users.noreply.github.com>
2017-10-10 17:09:36 +0200

    default values set

--- configScript.js
+++ configScript.js
@@ -1,15 +1,45 @@
 ﻿$(document).ready(function(){
-    $.each($('h3'), function(index, value){
-        var name = ($('h3').eq(index).next().find('tbody').first('tr').first('td').find('code').text());
-        console.log(name);
+    var headers = $('h3').slice(0,28);
+    $.each(headers, function(index, value){
+        var firstRow= headers.eq(index).next().find('tbody').first('tr');
+
+        var name = firstRow.first('td').find('code').text();
+        var values = firstRow.find('td').eq(2).text().split(':');
+
         $('h3').eq(index).after(getText(name));
+
+        setDefaults(name, values);
+
+        printResults(name);
     })
 });
 
 function getText(name){
-var text=`<label>Value</label>
- <input type="radio" name="` + name + `" value="true" checked>True</input>
- <input type="radio" name="` + name + `" value="false">False</input>`
+var text=` 
+ <label>Value</label>
+ <div id=`+ name +`>
+    <input type="radio" name="` + name + `" value="true"/>True
+    <input type="radio" name="` + name + `" value="false"/>False
+ </div>
 
+ <label>Severity</label>
+ <div id=`+ name +`-level>
+    <input type="radio" name="` + name + `-level" value="none"/>None
+    <input type="radio" name="` + name + `-level" value="suggestion"/>Suggestion
+    <input type="radio" name="` + name + `-level" value="warning"/>Warning
+    <input type="radio" name="` + name + `-level" value="error"/>Error
+ </div>
+ `
  return text;
+}
+
+function setDefaults(name, values){
+        $('input[name="' + name +'"][value="' + values[0] + '"]').prop('checked', true);
+        $('input[name="' + name +'-level"][value="' + values[1] + '"]').prop('checked', true);
+}
+
+function printResults(name){
+        var resultValue = $('input[name="' + name +'"]:checked').val();
+        var resultLevel = $('input[name="' + name +'-level"]:checked').val();
+        console.log(name + '=' + resultValue +':' + resultLevel);
 }
