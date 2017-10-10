f6de2e705447a01c18aa0b02908ad85241fab749
Margi <Margi47@users.noreply.github.com>
2017-10-10 12:57:33 +0200

    Adding true/false buttons with right names

---
+++ configScript.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function(){
+    $.each($('h3'), function(index, value){
+        var name = ($('h3').eq(index).next().find('tbody').first('tr').first('td').find('code').text());
+        console.log(name);
+        $('h3').eq(index).after(getText(name));
+    })
+});
+
+function getText(name){
+var text=`<label>Value</label>
+ <input type="radio" name="` + name + `" value="true" checked>True</input>
+ <input type="radio" name="` + name + `" value="false">False</input>`
+
+ return text;
+}
