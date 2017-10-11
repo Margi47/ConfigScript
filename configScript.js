d397ac8bcfc5f0cb712c4bc71ff4c2dc43ccb5be
Margi <Margi47@users.noreply.github.com>
2017-10-11 12:04:18 +0200

    setting defaults for standart cases

--- configScript.js
+++ configScript.js
@@ -26,58 +26,56 @@
 });
 
 function getText(name, values, hasLevel){
-var options ="";
+    var options ="";
+    $.each(values, function(index, value){
+        options += '<input type="radio" name="' + name + '" value="' + value.toLowerCase() + '"/>' + value;
+    });
 
-$.each(values, function(index, value){
-  options += '<input type="radio" name="' + name + '-level" value="' + value.toLowerCase() + '"/>' + value;
-});
+    var text ='<label>Value</label><div>' + options + '</div>';
+    var levelText =`
+        <label>Severity</label>
+        <div>
+            <input type="radio" name="` + name + `-level" value="none"/>None
+            <input type="radio" name="` + name + `-level" value="suggestion"/>Suggestion
+            <input type="radio" name="` + name + `-level" value="warning"/>Warning
+            <input type="radio" name="` + name + `-level" value="error"/>Error
+        </div>`;
 
-console.log(options);
-var text ='<label>Value</label><div>' + options + '</div>';
-
-var levelText =`
- <label>Severity</label>
- <div>
-    <input type="radio" name="` + name + `-level" value="none"/>None
-    <input type="radio" name="` + name + `-level" value="suggestion"/>Suggestion
-    <input type="radio" name="` + name + `-level" value="warning"/>Warning
-    <input type="radio" name="` + name + `-level" value="error"/>Error
- </div>
- `;
-
- if(hasLevel){
-    text += levelText;
- }
-
- return text;
+    if(hasLevel){
+        text += levelText;
+    }
+    return text;
 }
 
 function createButton(){
-var button=`
-<div id="resultButton">
-Get Results
-</div>
-`
+    var button=`
+    <div id="resultButton">
+        Get Results
+    </div>
+    `
 
-$('body').first().prepend(button);
-$('#resultButton').css({
-    'position':'fixed', 
-    'top': '0', 
-    'right':'0', 
-    'border':'3px solid red',
-    'font-size':'40px',
-    'background-color':'white',
-    'padding':'5px'
+    $('body').first().prepend(button);
+    $('#resultButton').css({
+        'position':'fixed', 
+        'top': '0', 
+        'right':'0', 
+        'border':'3px solid red',
+        'font-size':'40px',
+        'background-color':'white',
+        'padding':'5px'
     });
 }
 
 function setDefaults(name, values){
-        $('input[name="' + name +'"][value="' + values[0] + '"]').prop('checked', true);
+    $('input[name="' + name +'"][value="' + values[0] + '"]').prop('checked', true);
+
+    if(values[1] != undefined){
         $('input[name="' + name +'-level"][value="' + values[1] + '"]').prop('checked', true);
+    }
 }
 
 function printResults(name){
-        var resultValue = $('input[name="' + name +'"]:checked').val();
-        var resultLevel = $('input[name="' + name +'-level"]:checked').val();
-        console.log(name + '=' + resultValue +':' + resultLevel);
+    var resultValue = $('input[name="' + name +'"]:checked').val();
+    var resultLevel = $('input[name="' + name +'-level"]:checked').val();
+    console.log(name + '=' + resultValue +':' + resultLevel);
 }
