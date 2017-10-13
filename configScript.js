3c92d11eeda95401abd7937b78fd25af30caec99
Margi <Margi47@users.noreply.github.com>
2017-10-13 13:50:32 +0200

    methods refactored

--- configScript.js
+++ configScript.js
@@ -3,17 +3,12 @@
 
     var headers = $('h1').eq(1).nextUntil($('h1').eq(5),'h3');
     $.each(headers, function(index, value){
-        var firstRow= headers.eq(index).next().find('tbody').first('tr');
-        var name = firstRow.first('td').find('code').text();
-        var defaultValues = firstRow.find('td').eq(2).text().split(':');
+        var header = headers.eq(index);
+        var name = getName(header);
+        var defaultValues = getDefaults(header);
+        var possibleValues = getPossibleValues(header);
 
-        var values=[];
-        var possibleValues = headers.eq(index).nextUntil('h3','table').eq(1).find('tbody').find('tr');
-        $.each(possibleValues, function(index, value){
-            values.push(possibleValues.eq(index).find('td').first().text());
-        })
-
-        headers.eq(index).after(getText(name, values, defaultValues[1] != undefined));
+        header.after(getText(name, possibleValues, defaultValues[1] != undefined));
         setDefaults(name, defaultValues);
     })
 
@@ -45,6 +40,23 @@
     });
 
 });
+
+function getName(header){
+    return header.nextUntil('h3','table').eq(0).find('tbody').first('tr').first('td').find('code').text();
+}
+
+function getDefaults(header){
+    return header.nextUntil('h3','table').eq(0).find('tbody').first('tr').find('td').eq(2).text().split(':');
+}
+
+function getPossibleValues(header){
+    var values=[];
+    var possibleValues = header.nextUntil('h3','table').eq(1).find('tbody').find('tr');
+    $.each(possibleValues, function(index, value){
+        values.push(possibleValues.eq(index).find('td').first().text());
+    });
+    return values;
+}
 
 function getText(name, values, hasLevel){
     var options ="";
@@ -124,8 +136,9 @@ function getResults(){
     var previousHeader = "";
     var headers = $('h1').eq(1).nextUntil($('h1').eq(5),'h3');
     $.each(headers, function(index, value){
-        var name = headers.eq(index).nextUntil('h3','table').first().find('tbody').first('tr').first('td').find('code').text();
-        var defaultSettings = headers.eq(index).nextUntil('h3','table').first().find('tbody').first('tr').find('td').eq(2).text().split(':');
+        var header = headers.eq(index);
+        var name = getName(header);
+        var defaultSettings = getDefaults(header);
         var resultValue = $('input[name="' + name +'"]:checked').val();
         var resultLevel = $('input[name="' + name +'-level"]:checked').val();
 
